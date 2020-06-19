dde137013a8b77804f4a39b459df3f6a03a07ee2
therinz <50236992+therinz@users.noreply.github.com>
2020-06-19 23:48:45 +0200

    clean up and rewording of certain classes and funcs

--- datastructures.py
+++ datastructures.py
@@ -1,17 +1,71 @@
-from helpers import get_rostercodes, get_airports, import_airport_data
+import csv
+from datetime import timedelta
+
 from geopy.distance import great_circle
 
 
-class AirportNotKnown(Exception):
-    def __init__(self, airport_list):
-        self.airport_list = airport_list
+def get_rostercodes():
+    """ Load roster codes from csv. """
 
-    def __str__(self):
-        return "Some airports are not defined: " + " ".join(self.airport_list)
+    with open("other_duties.csv", "r", newline="") as f:
+        contents = csv.reader(f, delimiter="|")
+        return {row[0]: [row[1], row[2], row[3]]
+                for row in contents}
+
+
+def get_airports():
+    """ Load airports from frequent airports csv. """
+
+    with open("airports.csv", "r", newline="") as file:
+        contents = csv.DictReader(file, delimiter="|")
+        contents = sorted(contents, key=lambda row: row["Name"])
+
+        return {row["IATA"]: Airport(row["IATA"],
+                                     row["ICAO"],
+                                     row["Name"],
+                                     (row["LAT"], row["LONG"]))
+                for row in contents}
+
+
+def time_diff(a, b):
+    # TODO Does not take into account if same time different day
+    def transpose(x): return int("".join([x[0:2], x[3:5]]))
+    day = 0 if transpose(a) <= transpose(b) else 1
+    a = a.split(":")
+    b = b.split(":")
+    return (timedelta(days=day, hours=int(b[0]), minutes=int(b[1]))
+            - timedelta(days=0, hours=int(a[0]), minutes=int(a[1])))
+
+
+def import_airport_data(IATA, write=1):
+    """ Lookup airport data in big list and save in selected airport list. """
+
+    with open("all_airports.csv", "r", newline="", encoding='utf-8') as file:
+        contents = csv.DictReader(file)
+
+        for row in contents:
+            if row["iata_code"] == IATA.upper():
+                apd = Airport(row["iata_code"],
+                              row["gps_code"],
+                              row["municipality"],
+                              (row["latitude_deg"], row["longitude_deg"]))
+                break
+            else:
+                raise AirportNotKnown([IATA])
+
+        if write == 1:
+            with open("airports.csv", "a", newline="") as file2:
+                file_writer = csv.writer(file2, delimiter="|")
+                file_writer.writerow([apd.iata,
+                                      apd.icao,
+                                      apd.name,
+                                      apd.coord[0], apd.coord[1]])
+
+        return apd
 
 
 class Airport:
-    """ Create airport to call in various instances. """
+    """ Class to store data relating to an airport. """
 
     def __init__(self, iata, icao, name, coord):
         self.iata = iata
@@ -20,10 +74,11 @@ class Airport:
         self.coord = coord
 
     def __str__(self):
-        return "{} ({}) is located at {}.".format(self.name,
-                                                  self.iata,
-                                                  (", ".join(map(str,
-                                                                 self.coord))))
+        return ("{} ({}) is located at {}."
+                .format(self.name, self.iata,
+                        (", ".join(map(str, self.coord)))
+                        )
+                )
 
 
 class DutyDay:
@@ -36,13 +91,17 @@ class DutyDay:
         self.duties = duties
         self.start_time = report_time
         self.end_time = off_duty  # might be after midnight, thus < std
-        # standby start will be before report time
+        # TODO standby start will be before report time
 
 
 class Flight:
-    """ Flight used to distance between 2 places on duty day. """
+    """ Flight used to distance between 2 airports. """
+
+    # Populate list with frequent EZY airports
     airports_list = get_airports()
     simulators = ["XBH", "XCS", "XDH", "XWT", "XSW", "XOL"]
+    # Factored length of duty, multiplied by 10
+    nominal = {"s": 8, "m": 12, "l": 15, "xl": 25}
 
     def __init__(self, flight_no, dep, arr, std, sta,
                  position=False, comeback=False):
@@ -53,11 +112,14 @@ class Flight:
         self.position = position
         self.sta = sta  # might be after midnight, thus < std
         self.sta = std
+
+        # If airport data not known, import from big list
         for IATA in self.dep, self.arr:
             if not Flight.airports_list.get(IATA):
                 Flight.airports_list[IATA] = import_airport_data(IATA)
 
     def distance(self):
+        # Skip the calculation if no take off
         if self.comeback == "R":
             sector = "Ground return"
             length = 0
@@ -78,6 +140,7 @@ class Flight:
 
 class OtherDuty:
     """ Create a class of any duty other than a flight. """
+
     rostercodes = get_rostercodes()
 
     def __init__(self, rostercode, start_time=None, end_time=None):
@@ -86,3 +149,86 @@ class OtherDuty:
         self.end_time = end_time
 
 
+class AirportNotKnown(Exception):
+    """ Custom message if number of airports not known. """
+
+    def __init__(self, airport_list):
+        self.airport_list = airport_list
+
+    def __str__(self):
+        return "Some airports are not defined: " + " ".join(self.airport_list)
+
+
+# FUNCTIONS BELOW NOT USED
+
+def create_new_airport():
+    """ Ask user for airport data and export it to airports.csv. """
+
+    print("Create a new airport with the next 5 questions")
+    iata = validate_input("Type 3 letter IATA code", str.upper, 3, 3)
+    airports_dict = get_airports()
+    if airports_dict.get(iata):
+        return f"Airport {iata} already in list"
+    icao = validate_input("Type 4 letter ICAO code", str.upper, 4, 4)
+    name = validate_input("Type name of airport", str.title, 2, 30)
+    lat = validate_input("Type latitude as 1.234567", float)
+    long = validate_input("Type longitude as 1.234567", float)
+    with open("airports.csv", "a", newline="") as file:
+        filewriter = csv.writer(file, delimiter="|")
+        filewriter.writerow([iata, icao, name, lat, long])
+    return Airport(iata, icao, name, (lat, long))
+
+
+def check_flight_length():
+    """ Ask user for 2 airports and print great circle distance. """
+
+    apt_a = validate_input("Type first IATA", str.upper, 3, 3)
+    apt_b = validate_input("Type second IATA code", str.upper, 3, 3)
+    airports_dict = get_airports()
+    distance = int(great_circle(airports_dict[apt_a].coord,
+                                airports_dict[apt_b].coord)
+                   .nautical)
+    print("The distance between {0} and {1} is {2} nautical miles."
+          .format(airports_dict[apt_a].name,
+                  airports_dict[apt_b].name,
+                  distance))
+
+
+def list_airports():
+    airports_dict = get_airports()
+    print("Available airports are:")
+    for airport in airports_dict.values():
+        print(f"{airport.name} ({airport.iata})")
+
+
+def validate_input(prompt, type_=None, min_=None, max_=None):
+    """ Request user input and clean it before return.
+
+    :param prompt: Question to ask user.
+    :param type_: Type of value asked. str, int, float.
+    :param min_: Minimum length of str of lower value of int.
+    :param max_: Maximum length of str of upper value of int.
+    :return: str, int or float.
+    """
+    if (min_ is not None
+            and max_ is not None
+            and max_ < min_):
+        raise ValueError("min_ must be less than or equal to max_.")
+    while True:
+        ui = input(prompt)
+        if type_ is not None:
+            try:
+                ui = type_(ui)
+            except ValueError:
+                print(f"Input type must be {type_.__name__}")
+                continue
+        if isinstance(ui, str):
+            ui_num = len(ui)
+        else:
+            ui_num = ui
+        if max_ is not None and ui_num > max_:
+            print(f"Input must be less than or equal to {max_}.")
+        elif min_ is not None and ui_num < min_:
+            print(f"Input must be more than or equal to {min_}.")
+        else:
+            return ui
--- process.py
+++ process.py
@@ -3,12 +3,15 @@ import re
 from datetime import timedelta
 from bs4 import BeautifulSoup
 
-from datastructures import DutyDay, Flight, OtherDuty
-from helpers import get_rostercodes, time_diff
+from datastructures import DutyDay, Flight, OtherDuty, get_rostercodes
+from datastructures import time_diff
+
+GND_POS = ["OWN", "TAXI", "TRN", "NSO"]
 
 
-class ConvertDutyDay:
-    """ Read list of items on day of roster and return DutyDay with each Duty item.
+class ParseRoster:
+    """
+    Read list of items on day of roster and return DutyDay with each Duty item.
 
     :param self.lv: Store local values for working on roster decryption
     :type self.lv: dict
@@ -16,18 +19,19 @@ class ConvertDutyDay:
             and/or OtherDuty and optional report & end times
     """
     roster_codes = get_rostercodes()
-    timed_roster_codes = [code for code, values in roster_codes.items()
+    timed_roster_codes = [code
+                          for code, values in roster_codes.items()
                           if values[0] == "True"]
-    ground_pos_codes = ["OWN", "TAXI", "TRN", "NSO"]
 
     def __init__(self):
+        self.continued_duty = False
         self.lv = {}
         self.duties, self.days = [], []
 
     def search_duty_type(self, row):
         search_flight_number = re.search(r"[0-9]{3,4}", row)
         search_non_flight = re.search(r"[A-Z/]{3,4}", row)
-        if row in ConvertDutyDay.ground_pos_codes:
+        if row in GND_POS:
             # Takes into account taxi between bases.
             self.lv["position"] = "ground"
             self.lv["flight_number"] = True
@@ -39,28 +43,29 @@ class ConvertDutyDay:
                 self.clean_up()
         elif search_non_flight and not self.lv.get("flight_number"):
             self.lv["other_duty"] = search_non_flight.group()
-            if self.lv["other_duty"] not in ConvertDutyDay.timed_roster_codes:
+            if self.lv["other_duty"] not in ParseRoster.timed_roster_codes:
                 self.lv["no_time"] = True
 
     def time_details(self, search_time, prev):
-        time_type = None
+        """ Interpret current time type by switching after the previous. """
+
+        switch = {"report_time": "STD",
+                  "STD": "STA",
+                  "start_time": "end_time"}
+        # Flight in progress
         if self.lv.get("flight_number"):
             if not self.lv.get("report_time"):
                 time_type = "report_time"
-            elif prev == "report_time":
-                time_type = "STD"
-            elif prev == "STD":
-                time_type = "STA"
-        elif self.lv.get("other_duty"):
-            if prev == "start_time":
-                time_type = "end_time"
             else:
-                time_type = "start_time"
+                time_type = switch.get(prev)
+        # Timed duty other than a flight
+        elif self.lv.get("other_duty"):
+            time_type = switch.get(prev, "start_time")
+        # Flight after stby
         elif self.lv.get("last_time_type") == "end_time":
-            # flight after stby
             time_type = "report_time"
+        # After flight or flight-stby
         else:
-            # could be after flight or flight-stby
             time_type = "off_time"
         self.lv[time_type] = search_time
         self.lv["previous_item"] = time_type
@@ -74,49 +79,76 @@ class ConvertDutyDay:
             self.lv["arr"] = search_iata.group()
 
     def clean_up(self, end_of_duty=False, keep_duty_type=False):
+        """
+        Delete values in dictionary cache. Method may be called on start of new
+        day because previous day was unfinished, as set by keep_duty_type=True.
+
+        """
+
         save_vals = {}
+
+        # Duty has ended but isn't saved yet
         if end_of_duty:
             self.days.append(DutyDay(self.duties,
                                      report_time=self.lv.get("report_time"),
                                      off_duty=self.lv.get("off_time")))
             self.duties = []
+
+            # Keep_duty_type signals new duty has already begun,
+            # so save data from the new day.
             if keep_duty_type:
-                # After clearing lv, new info will be lost, so save before
                 save_vals.update({"flight_number": self.lv.get("flight_number"),
                                   "other_duty": self.lv.get("other_duty")})
+
+        # Element of duty has finished, not the day completely
         else:
             report = self.lv.get("report_time")
+
             if report:
                 save_vals.update({"report_time": report,
                                   "previous_item": "report_time"})
             previous = self.lv.get("previous_item")
-            no_time = self.lv.get("no_time")
+
+            # If last time > 9h end previous duty day
             if previous:
-                # If last time > 9h end previous duty day
                 save_vals.update({"last_time": self.lv[previous],
                                   "last_type": previous})
-            elif no_time:
+            elif self.lv.get("no_time"):
                 save_vals.update({"last_type": "no_time"})
+
+        # Clear local values and save new data
         self.lv.clear()
         self.lv.update(save_vals)
 
-    def process(self, day):
+    def day(self, day):
+        """ For one day, loop through all rows and extract duties and times. """
+
         i = 0
         for j, row in enumerate(day):
-            skip_vals = ["None", " EJU", " ", "Block", "Duty", " OWNA", "(320)", "(321)", "EZS", " EZS"]
+            skip_vals = ["None", " EJU", " ", "Block",
+                         "Duty", " OWNA", "(320)", "(321)", "EZS", " EZS"]
+
+            # Skip block & duty times at end of day
             if j > (len(day) - 4):
-                # compensate for block & duty times at end of day
                 break
+            # Skip empty and non-relevant rows
             elif row in skip_vals or len(row) < 3:
                 i += 1
+                # More than 3 empty rows signals day off
                 if i > 3:
-                    if len(self.duties) > 0 and self.lv.get("last_type") == "no_time":
+                    # There may still be an unfinished duty from the day prior.
+                    if (len(self.duties) > 0
+                            and self.lv.get("last_type") == "no_time"):
                         self.clean_up(end_of_duty=True)
                     break
                 continue
             i = 0
+
+            # Check what's happening on row
             self.search_duty_type(row)
             search_time = re.search(r"[0-9]{2}:[0-9]{2}", row)
+
+            # If current row is a time, check what type of activity
             if search_time:
                 previous = self.lv.get("previous_item")
                 time = search_time.group()
@@ -124,41 +156,52 @@ class ConvertDutyDay:
                               or self.lv.get("last_type") == "end_time")
                 if (unfinished and (time_diff(self.lv.get("last_time"), time)
                                     > timedelta(hours=9))):
+                    # New day but still values in cache, so save that first.
                     self.clean_up(end_of_duty=True, keep_duty_type=True)
                 self.time_details(time, previous)
-            elif self.lv.get("flight_number") and row not in ConvertDutyDay.ground_pos_codes:
+            elif self.lv.get("flight_number") and row not in GND_POS:
                 search_iata = re.search(r"[A-Z]{3}\Z", row)
                 if search_iata:
                     self.flight_details(row, search_iata)
                     continue
-            # Now when ready save found values in duties
+
             prev = self.lv.get("previous_item")
+            # When STA is set, all flight details are known
             if prev == "STA":
-                # With STA all flight details are known. Save as Flight in duties dict.
                 self.duties.append(Flight(self.lv["flight_number"],
                                           self.lv["dep"], self.lv["arr"],
                                           self.lv["STD"], self.lv["STA"],
                                           position=self.lv.get("position"),
                                           comeback=self.lv.get("comeback")))
                 self.clean_up()
+
+            # If not a flight but end/no time set, save as OtherDuty
             elif prev == "end_time" or self.lv.get("no_time"):
-                # Some non-flights have start and end times, others don't. Save as OtherDuty in duties dict.
-                self.duties.append(OtherDuty(self.lv["other_duty"],
-                                             start_time=self.lv.get("start_time"),
-                                             end_time=self.lv.get("end_time")))
+                self.duties.append(
+                    OtherDuty(self.lv["other_duty"],
+                              start_time=self.lv.get("start_time"),
+                              end_time=self.lv.get("end_time"))
+                )
                 self.clean_up()
+
+            # Off time set means end of of day so clean up
             if self.lv.get("off_time"):
                 self.clean_up(end_of_duty=True)
 
-    def day_for_day(self, month):
+    def full_period(self, month):
+        """ Parse roster for every day in given period (month) """
+
         for day in month:
-            self.process(day)
+            self.day(day)
+
+        # It might be that last duty was unfinished
         if len(self.duties) > 0:
+            self.continued_duty = True  # TODO Finish up if last day of period
             self.clean_up(end_of_duty=True)
 
 
 class ReadRoster:
-    """ Open and read html roster. """
+    """ Parse html roster. """
 
     def __init__(self, source):
         self.source = source
@@ -170,11 +213,12 @@ class ReadRoster:
         else:
             self.print_roster()
 
-    def read_workdays(self):
+    def parse_html(self):
         """ Find every line in html roster with duty element.
 
         :return: Nested list with rows per column.
         """
+
         rows = []
         for row in self.soup.find_all("tr"):
             cells = [str(cell.string) for cell in row.find_all("td")]
@@ -183,20 +227,26 @@ class ReadRoster:
         return [[row[i] for row in rows] for i in range(32)]
 
     def night_stops(self):
-        search_hotel = self.soup.find(string=re.compile(r"[A-Za-z]{6,12} HOTEL"))
-        if search_hotel is not None:
-            return re.findall(r"[A-Z][a-z]{2}[0-9]{2}", str(search_hotel.parent))
+        s = self.soup.find(string=re.compile(r"[A-Za-z]{6,12} HOTEL"))
+        if s is not None:
+            return re.findall(r"[A-Z][a-z]{2}[0-9]{2}", str(s.parent))
 
     def print_roster(self):
-        nominal = {"s": 8, "m": 12, "l": 15, "xl": 25}
-        total = num_flights = positioning = asby = ground_duties = domestic = days_flying = days_at_work = 0
+        """ Print roster for one file """
+
+        total = num_flights = positioning = asby = 0
+        ground_duties = domestic = days_flying = days_at_work = 0
         off_duty = {"LVE": 0, "SICK": 0, "ULV": 0}
-        roster_days = ConvertDutyDay()
-        roster_days.day_for_day(self.read_workdays())
-        for i, duty_day in enumerate(roster_days.days, 1):
+        roster = ParseRoster()
+        roster.full_period(self.parse_html())
+
+        for i, duty_day in enumerate(roster.days, 1):
             on_asby = multi_pos = multi_gd = at_work = False
+
+            # Skip empty days
             if duty_day is None:
                 continue
+
             print(f"Day {i}")
             for duty in duty_day.duties:
                 if isinstance(duty, Flight):
@@ -210,8 +260,8 @@ class ReadRoster:
                                 ((duty.dep not in Flight.simulators
                                   and duty.arr not in Flight.simulators)
                                  or distance > 15)):
-                            # Length not taken into account, only 1 leg per day
-                            # Ground pos to LGW and MXP not properly calculated
+                            # TODO Length not taken into account, only 1 leg per day
+                            # TODO Ground pos to LGW and MXP not properly calculated
                             positioning += 1
                         multi_pos = True
                         print("    {} positioning duty from {} to {}"
@@ -221,7 +271,7 @@ class ReadRoster:
                         print(f"    (This flight in {duty.dep} returned "
                               f"to stand while still on ground.)")
                     else:
-                        total += nominal[sector]
+                        total += Flight.nominal[sector]
                         num_flights += 1
                         icao_d = duty.airports_list[duty.dep].icao
                         icao_a = duty.airports_list[duty.arr].icao
@@ -234,7 +284,9 @@ class ReadRoster:
                             on_asby = False
                 else:
                     roster_codes = get_rostercodes()
-                    paid_codes = [code for code, values in roster_codes.items() if values[2] == "True"]
+                    paid_codes = [code
+                                  for code, values in roster_codes.items()
+                                  if values[2] == "True"]
                     if duty.rostercode in paid_codes and not multi_gd:
                         if not at_work:
                             days_at_work += 1
@@ -249,7 +301,8 @@ class ReadRoster:
                             days_at_work += 1
                             at_work = True
                         on_asby = True
-                        if time_diff(duty.start_time, duty.end_time) < timedelta(hours=4):
+                        if (time_diff(duty.start_time, duty.end_time)
+                                < timedelta(hours=4)):
                             asby += 1
                         else:
                             asby += 2
@@ -269,79 +322,8 @@ class ReadRoster:
         night_stops = self.night_stops()
         if night_stops is not None:
             nights = ", ".join(night_stops)
-            print("\nNightstopping {} times: {}".format(len(night_stops), nights))
-
-    # def year_stats(self):
-    #     nominal = {"s": 8, "m": 12, "l": 15, "xl": 25}
-    #     total = num_flights = positioning = asby = ground_duties = domestic = 0
-    #     off_duty = {"LVE": 0, "SICK": 0, "ULV": 0}
-    #
-    #     roster_days = ConvertDutyDay()
-    #     roster_days.day_for_day(self.read_workdays())
-    #     for i, duty_day in enumerate(roster_days.days, 1):
-    #         on_asby = multi_pos = multi_gd = False
-    #         if duty_day is None:
-    #             continue
-    #         print(f"Day {i}")
-    #         for duty in duty_day.duties:
-    #             if isinstance(duty, Flight):
-    #                 distance, sector = duty.distance()
-    #                 if duty.position:
-    #                     if (not multi_pos and
-    #                             ((duty.dep not in Flight.simulators
-    #                               and duty.arr not in Flight.simulators)
-    #                              or distance > 15)):
-    #                         # Length not taken into account, only 1 leg per day
-    #                         # Ground pos to LGW and MXP not properly calculated
-    #                         positioning += 1
-    #                     multi_pos = True
-    #                     print("    {} positioning duty from {} to {}"
-    #                           .format(str(duty.position).capitalize(),
-    #                                   duty.dep, duty.arr))
-    #                 elif duty.comeback == "R":
-    #                     print(f"    (This flight in {duty.dep} returned "
-    #                           f"to stand while still on ground.)")
-    #                 else:
-    #                     total += nominal[sector]
-    #                     num_flights += 1
-    #                     icao_d = duty.airports_list[duty.dep].icao
-    #                     icao_a = duty.airports_list[duty.arr].icao
-    #                     if icao_d.startswith("LF") and icao_a.startswith("LF"):
-    #                         domestic += 1
-    #                     print(f"    Flight from {duty.dep} to {duty.arr}, "
-    #                           f"length {distance} ({sector}).")
-    #                     if on_asby:
-    #                         asby -= 1
-    #                         on_asby = False
-    #             else:
-    #                 roster_codes = get_rostercodes()
-    #                 paid_codes = [code for code, values in roster_codes.items() if values[2] == "True"]
-    #                 if duty.rostercode in paid_codes and not multi_gd:
-    #                     ground_duties += 1
-    #                     multi_gd = True
-    #                 for item in off_duty.keys():
-    #                     if duty.rostercode == item:
-    #                         off_duty[item] += 1
-    #                 if duty.rostercode == "ASBY":
-    #                     on_asby = True
-    #                     if time_diff(duty.start_time, duty.end_time) < timedelta(hours=4):
-    #                         asby += 1
-    #                     else:
-    #                         asby += 2
-    #                 print(f"    Duty with code {duty.rostercode}")
-    #     paid_items = {"nominal sector": (total / 10),
-    #                   "number of flights": num_flights,
-    #                   "French domestic flights": domestic,
-    #                   "positioning duties": positioning,
-    #                   "airport standby": asby,
-    #                   "paid ground duties": ground_duties}
-    #     paid_items.update(off_duty)
-    #     for item, count in paid_items.items():
-    #         if count > 0:
-    #             print(f"Total {item} count is {count}.")
-    #     night_stops = self.night_stops()
-    #     if night_stops is not None:
-    #         nights = ", ".join(night_stops)
-    #         print("\nNightstopping {} times: {}".format(len(night_stops), nights))
+            print(f"\nNightstopping {len(night_stops)} times: {nights}")
 
 
+if __name__ == '__main__':
+    ReadRoster(rf"C:\Users\Rinze\Documents\Werk\rooster\2019\19-01.htm")
